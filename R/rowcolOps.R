38c4dab54b7958eefa0d5e7cc2054059df299be0
Gregor Kappler <g.kappler@gmx.net>
2016-08-23 23:55:33 +0200

    aggregationOps with margin

--- R/rowcolOps.R
+++ R/rowcolOps.R
@@ -1,4 +1,4 @@
-rowOps<-function(object,operation,...)
+aggregationOps<-function(object,margin,operation,...)
 {
 	connection<-getConnection(object)
 	flag3Check(connection)
@@ -12,12 +12,12 @@ rowOps<-function(object,operation,...)
 	else stop("Please enter either \"Sum\" or \"Mean\"")
 
 	sqlstr<-paste0( " SELECT '%insertIDhere%' AS vectorIdColumn ",#getMaxVectorId(connection),
-			        ",",var,".",object@dimColumns[[1]]," AS vectorIndexColumn",
+			        ",",var,".",object@dimColumns[[margin]]," AS vectorIndexColumn",
 			        ", ",opt,"(",var,".",object@dimColumns[[3]],") AS vectorValueColumn 
 					FROM ",
 					"( ",constructSelect(object),
 					" ) AS ",var,
-					" GROUP BY ",var,".",object@dimColumns[[1]])
+					" GROUP BY ",var,".",object@dimColumns[[margin]])
 
 	tblfunqueryobj <- new("FLTableFunctionQuery",
                         connection = connection,
@@ -34,48 +34,5 @@ rowOps<-function(object,operation,...)
 								"vectorValueColumn"),
 				isDeep = FALSE)
 
-	return(ensureQuerySize(pResult=flv,
-	            pInput=list(object),
-	            pOperator="rowOps"))
-}
-
-colOps<-function(object,operation,...)
-{
-	connection<-getConnection(object)
-	flag3Check(connection)
-	var <- genRandVarName()
-
-	if (operation=="Sum")
-		opt<-"SUM"
-	else if(operation=="Mean")
-		opt<-"AVG"
-
-	else stop("Please enter either \"Sum\" or \"Mean\"")
-
-	sqlstr<-paste0( " SELECT '%insertIDhere%' AS vectorIdColumn ",#getMaxVectorId(connection),
-			        ",",var,".",object@dimColumns[[2]]," AS vectorIndexColumn",
-			        ", ",opt,"(",var,".",object@dimColumns[[3]],") AS vectorValueColumn 
-					FROM ",
-					"( ",constructSelect(object),
-					" ) AS ",var,
-					" GROUP BY ",var,".",object@dimColumns[[2]])
-
-	tblfunqueryobj <- new("FLTableFunctionQuery",
-                        connection = connection,
-                        variables = list(
-			                obs_id_colname = "vectorIndexColumn",
-			                cell_val_colname = "vectorValueColumn"),
-                        whereconditions="",
-                        order = "",
-                        SQLquery=sqlstr)
-
-	flv <- new("FLVector",
-				select = tblfunqueryobj,
-				dimnames = list(1:nrow(object),
-								"vectorValueColumn"),
-				isDeep = FALSE)
-
-	return(ensureQuerySize(pResult=flv,
-	            pInput=list(object),
-	            pOperator="colOps"))
+	return(flv)
 }
